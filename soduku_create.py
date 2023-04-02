94e31d746cfa884256877e17a99c001f0918a14b
NguyenThang1501 <109154036+NguyenThang1501@users.noreply.github.com>
2023-04-02 23:52:49 +0700

    Create soduku_create.py

---
+++ soduku_create.py
@@ -0,0 +1,82 @@
+import pygame
+import requests
+
+WIDTH = 550
+background_color = (251,247,245)
+original_grid_element_color = (52,31,151)
+buffer = 5
+
+grid  = [[0,0,1,0,0,0,0,0,0],
+        [2,0,0,0,0,0,0,7,0],
+        [0,7,0,0,0,0,0,0,0],
+        [1,0,0,4,0,6,0,0,7],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,1,2,5,4,6],
+        [3,0,2,7,6,0,9,8,0],
+        [0,6,4,9,0,3,0,0,1],
+        [9,8,0,5,2,1,0,6,0]]
+grid_original = grid
+
+def insert(win, position):
+    i,j = position[1], position[0]
+    myfont = pygame.font.SysFont('Comic Sans MS', 35)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            if event.type == pygame.KEYDOWN:
+                if(grid_original[i-1][j-1] != 0):
+                    return
+                if(event.key == 48): #checking with 0
+                    grid[i-1][j-1] = event.key - 48
+                    pygame.draw.rect(win, background_color, (position[0]*50 + buffer, position[1]*50+ buffer,50 -2*buffer , 50 - 2*buffer))
+                    pygame.display.update()
+                    return
+                if(0 < event.key - 48 <10):  #We are checking for valid input
+                    pygame.draw.rect(win, background_color, (position[0]*50 + buffer, position[1]*50+ buffer,50 -2*buffer , 50 - 2*buffer))
+                    value = myfont.render(str(event.key-48), True, (0,0,0))
+                    win.blit(value, (position[0]*50 +15, position[1]*50))
+                    grid[i-1][j-1] = event.key - 48
+                    pygame.display.update()
+                    return
+                return
+
+def main():
+    pygame.init()
+    win = pygame.display.set_mode((WIDTH, WIDTH))
+    pygame.display.set_caption("Sudoku")
+    
+    win.fill(background_color)
+    myfont = pygame.font.SysFont('Comic Sans MS', 35)
+    
+    for i in range(0,10):
+        if(i %3==0):
+            pygame.draw.line(win, (0,0,0), (50 + 50*i, 50), ( 50 + 50*i ,500), 4)
+            pygame.draw.line(win, (0,0,0), (50, 50 + 50*i), (500, 50 + 50*i), 4)
+        
+        pygame.draw.line(win, (0,0,0), (50 + 50*i, 50), ( 50 + 50*i ,500), 2)
+        pygame.draw.line(win, (0,0,0), (50, 50 + 50*i), (500, 50 + 50*i), 2)
+    
+    pygame.display.update()
+    
+    for i in range(0,len(grid[0])):
+        for j in range(0, len(grid[0])):
+            if (grid[i][j] >0):
+                value=myfont.render(str(grid[i][j]), True, original_grid_element_color)
+                win.blit(value, ((j+1)*50 + 15, (i+1)*50))
+    pygame.display.update()  
+    
+    while True: 
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                x = int(pygame.mouse.get_pos()[0] / WIDTH) * WIDTH
+                y = int(pygame.mouse.get_pos()[1] / WIDTH) * WIDTH
+                if pygame.mouse.get_pressed()[0]:
+                    pygame.draw.rect(s, (255,0,0), (x, y, WIDTH, WIDTH), 0)
+                pos = pygame.mouse.get_pos()
+                insert(win, (pos[0]//50, pos[1]//50))
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+        
+main()
